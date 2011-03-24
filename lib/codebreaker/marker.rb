47d04bf999c92dc725236b3e3eb61bd4c4fc93df
Charles Feduke <charles.feduke@gmail.com>
2011-03-24 07:46:13 -0400

    chapter 9

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -11,12 +11,21 @@ module Codebreaker
     end
 
     def number_match_count
-      (0..3).inject(0) do |count, index|
-        count + (number_match?(@guess, index) ? 1 : 0)
+      total_match_count - exact_match_count
+    end
+    
+    def total_match_count
+      secret = @secret.split('')
+      @guess.split('').inject(0) do |count, n|
+        count + (delete_first(secret, n) ? 1 : 0)
       end
     end
     
     private
+
+    def delete_first(code, n)
+      code.delete_at(code.index(n)) if code.index(n)
+    end
 
     def exact_match?(guess, index)
       guess[index] == @secret[index]
